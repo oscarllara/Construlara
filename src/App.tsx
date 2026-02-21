a9932c2c9c06ca2991efe462312e2aa93404f3d1
[dyad] <oscar.gas35@gmail.com>
2026-02-20 22:58:55 -0300

    [dyad] Restaurando o foco no projeto Construlara e desfazendo alterações de outro app - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Products from "./pages/Products";
+import Equipments from "./pages/Equipments";
+import Rentals from "./pages/Rentals";
+import Users from "./pages/Users";
+import Reports from "./pages/Reports";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ChatRoom from "./pages/ChatRoom";
+import ChangePassword from "./pages/ChangePassword";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,10 +24,23 @@ const App = () => (
       <Toaster position="top-center" />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          {/* Rotas Principais Construlara */}
+          <Route path="/" element={<Products />} />
+          <Route path="/loja" element={<Products />} />
+          <Route path="/equipamentos" element={<Equipments />} />
+          <Route path="/alugueis" element={<Rentals />} />
+          <Route path="/usuarios" element={<Users />} />
+          <Route path="/relatorios" element={<Reports />} />
+          <Route path="/carrinho" element={<Cart />} />
+          <Route path="/perfil" element={<Profile />} />
+          
+          {/* Institucional e Segurança */}
+          <Route path="/contato" element={<Contact />} />
+          <Route path="/privacidade" element={<Privacy />} />
+          <Route path="/termos" element={<Terms />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Register />} />
-          <Route path="/sala/:id" element={<ChatRoom />} />
+          <Route path="/alterar-senha" element={<ChangePassword />} />
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
